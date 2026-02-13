748e5758ee71121a89bc02c8bb1a77cf5908db1d
Milo <milo@edufunds.org>
2026-02-13 10:06:53 +0000

    fix: Cron-Job, 59 Datenfehler behoben, 6 Links repariert, Dev-Server Warnungen behoben

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  experimental: { serverComponentsExternalPackages: ['pg'] },
+  serverExternalPackages: ['pg'],
   async headers() {
     return [
       // Statische Assets mit langem Cache
